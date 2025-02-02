1417327190a13d4510a60d38907e177c5f53a4b2
Anuj-Pratap-Singh-12 <anujpratap4595@gmail.com>
2025-02-02 22:07:22 +0530

    Added project files

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+const Header = () => {
+  return (
+    <div className="p-2 shadow-sm">
+      <img src="./logo.svg"/>
+      <div>
+      <button>Sign in</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/create-trip/index.jsx
@@ -0,0 +1,9 @@
+
+
+const CreateTrip = () => {
+  return (
+    <div>Create trip</div>
+  )
+}
+
+export default CreateTrip
